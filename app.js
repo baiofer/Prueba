6336d8b948514df0392b615647c610b48d714f82
Fernando Jarilla <fjarilla@gmail.com>
2017-08-11 09:40:36 +0200

    Creada conexión a mongoDB

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 "use strict"
 
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
-var app = express();
+const db = require('./lib/connectDB');
+
+//Cargamos las definiciones de nuestros modelos
+// require('./models/Esquela');
+// require('./models/Localidad');
+// require('./models/LibroPesame');
+// require('./models/Condolencia');
+
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
